9ada3269d1a437e2ba92202bb5124eb590cdc8c1
Lucian Iernye <lucian.iernye@gmail.com>
2020-05-11 22:59:32 +0100

    erase function added

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-$(() => {
-  $("#slider").slider({
-    min: 3,
-    max: 30,
-    slide: function (event, ui) {
-      $("#circle").height(ui.value);
-      $("#circle").width(ui.value);
-    },
-  });
+$(function () {
+    $("#slider").slider({
+        min: 3,
+        max: 30,
+        slide: function (event, ui) {
+            $("#circle").height(ui.value);
+            $("#circle").width(ui.value);
+        },
+    });
 
-  /* ----------- example of drawing into canvas------------
+    /* ----------- example of drawing into canvas------------
 let canvas = document.getElementById("paint");
 let context = canvas.getContext("2d");
 
@@ -33,62 +33,71 @@ context.lineTo(400, 100);
 context.stroke();
 ---------------------------------------------------------*/
 
-  //   declare variables
-  //   painting, erasing or not
-  let paint = false;
-  //   painting or erasing
-  let paint_erase = "paint";
-  //   get the canvas and context
-  let canvas = document.getElementById("paint");
-  let ctx = canvas.getContext("2d");
-  //   get the canvas container
-  let container = $("#canvas-container");
-  //   mouse position
-  let mouse = { x: 0, y: 0 };
+    //   declare variables
+    //   painting, erasing or not
+    let paint = false;
+    //   painting or erasing
+    let paint_erase = "paint";
+    //   get the canvas and context
+    let canvas = document.getElementById("paint");
+    let ctx = canvas.getContext("2d");
+    //   get the canvas container
+    let container = $("#canvas-container");
+    //   mouse position
+    let mouse = {
+        x: 0,
+        y: 0,
+    };
 
-  //   onload load saved work from local strorage
+    //   onload load saved work from local strorage
 
-  // set drawing parameters (lineWidth, lineJoin, lineCap)
-  ctx.lineWidth = 3;
-  ctx.lineJoin = "round";
-  ctx.lineCap = "round";
+    // set drawing parameters (lineWidth, lineJoin, lineCap)
+    ctx.lineWidth = 3;
+    ctx.lineJoin = "round";
+    ctx.lineCap = "round";
 
-  //   click inside container
-  container.mousedown(function (event) {
-    paint = true;
-    ctx.beginPath();
-    mouse.x = event.pageX - this.offsetLeft;
-    mouse.y = event.pageY - this.offsetTop;
-    ctx.moveTo(mouse.x, mouse.y);
-  });
+    //   click inside container
+    container.mousedown(function (event) {
+        paint = true;
+        ctx.beginPath();
+        mouse.x = event.pageX - this.offsetLeft;
+        mouse.y = event.pageY - this.offsetTop;
+        ctx.moveTo(mouse.x, mouse.y);
+    });
 
-  //   move the mouse while holding mouse key
-  container.mousemove(function (event) {
-    mouse.x = event.pageX - this.offsetLeft;
-    mouse.y = event.pageY - this.offsetTop;
-    if (paint === true) {
-      if (paint_erase === "paint") {
-        // get color input
-        ctx.strokeStyle = "red";
-      } else {
-        // color will be white
-        ctx.strokeStyle = "white";
-      }
-      ctx.lineTo(mouse.x, mouse.y);
-      ctx.stroke();
-    }
-  });
+    //   move the mouse while holding mouse key
+    container.mousemove(function (event) {
+        mouse.x = event.pageX - this.offsetLeft;
+        mouse.y = event.pageY - this.offsetTop;
+        if (paint === true) {
+            if (paint_erase === "paint") {
+                // get color input
+                ctx.strokeStyle = "red";
+            } else {
+                // color will be white
+                ctx.strokeStyle = "white";
+            }
+            ctx.lineTo(mouse.x, mouse.y);
+            ctx.stroke();
+        }
+    });
 
-  //   mouse-up(when we don't hold the click anymore) -> we are not painting/erasing anymore
-  container.mouseup(function () {
-    paint = false;
-  });
+    //   mouse-up(when we don't hold the click anymore) -> we are not painting/erasing anymore
+    container.mouseup(function () {
+        paint = false;
+    });
 
-  // if we leave the container we are not painting/erasing anymore
-  container.mouseleave(function () {
-    paint = false;
-  });
+    // if we leave the container we are not painting/erasing anymore
+    container.mouseleave(function () {
+        paint = false;
+    });
 
-  //   click on the erase button
-  $("erase").click;
+    //   click on the erase button
+    $("#erase").click(function () {
+        if (paint_erase === "paint") {
+            paint_erase = "erase";
+        } else {
+            paint_erase = "paint";
+        }
+    })
 });
